d9f5594ad6776d6a8410b5759904f5f2d5d1200b
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2024-12-23 11:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 function makeid(l) {
   // write your code here
+	let e="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let ans="";
+
+	for(let i=0;i<l;i++)
+	{
+		let n = Math.random()*(52-0)+0;
+		ans+=e[Math.floor(n)];
+	}
+
+
+	return ans;
+	
+
+	
+	
+	
+	
 }
 
-// Do not change the code below.
+//Do not change the code below.
 const l = prompt("Enter a number.");
+
 alert(makeid(l));
+
